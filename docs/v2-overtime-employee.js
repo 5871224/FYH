42aec2e0c1f9d705540312376f3d16263b943c92
5871224 <110716199+5871224@users.noreply.github.com>
2026-07-06 15:49:43 +0800

    Publish V2 overtime UI loader

---
+++ docs/v2-overtime-employee.js
@@ -0,0 +1,6 @@
+(() => {
+  const script = document.createElement("script");
+  script.src = "https://cdn.jsdelivr.net/gh/5871224/FYH@7b8b678fbd9cd8029b4ed7ded4f5a121fc500bfb/src/renderer/v2-overtime-employee.js";
+  script.defer = false;
+  document.head.appendChild(script);
+})();
