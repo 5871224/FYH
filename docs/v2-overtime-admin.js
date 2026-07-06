730a602f5fe4209f4e3ca1e64eaeeab2c55a995a
5871224 <110716199+5871224@users.noreply.github.com>
2026-07-06 15:51:29 +0800

    Publish V2 overtime review UI loader

---
+++ docs/v2-overtime-admin.js
@@ -0,0 +1,6 @@
+(() => {
+  const script = document.createElement("script");
+  script.src = "https://cdn.jsdelivr.net/gh/5871224/FYH@462bf7cdf97891d92a7b9021c8d138c0cdd01efa/src/renderer/v2-overtime-admin.js";
+  script.defer = false;
+  document.head.appendChild(script);
+})();
