a972ce1a0a686db2778d89889be046e725fc1eac
5871224 <110716199+5871224@users.noreply.github.com>
2026-07-06 15:55:03 +0800

    Load published V2 meal UI

--- docs/v2-overtime-admin.js
+++ docs/v2-overtime-admin.js
@@ -1,6 +1,11 @@
 (() => {
-  const script = document.createElement("script");
-  script.src = "https://cdn.jsdelivr.net/gh/5871224/FYH@462bf7cdf97891d92a7b9021c8d138c0cdd01efa/src/renderer/v2-overtime-admin.js";
-  script.defer = false;
-  document.head.appendChild(script);
+  [
+    "https://cdn.jsdelivr.net/gh/5871224/FYH@462bf7cdf97891d92a7b9021c8d138c0cdd01efa/src/renderer/v2-overtime-admin.js",
+    "https://cdn.jsdelivr.net/gh/5871224/FYH@87ac3325d1b4574556b9ad5afa52c4b73cde64c0/src/renderer/v2-meal.js"
+  ].forEach((src) => {
+    const script = document.createElement("script");
+    script.src = src;
+    script.defer = false;
+    document.head.appendChild(script);
+  });
 })();
