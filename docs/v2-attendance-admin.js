fa07e9d57c5bc8b03a0e951c67e110f7d59f31c6
5871224 <110716199+5871224@users.noreply.github.com>
2026-07-06 16:12:14 +0800

    Use safe attendance admin loader

--- docs/v2-attendance-admin.js
+++ docs/v2-attendance-admin.js
@@ -1,1 +1,6 @@
-document.write('<script src="https://cdn.jsdelivr.net/gh/5871224/FYH@93370b20954f42d28f0c2666e071d36bf75bd961/src/renderer/v2-attendance-admin.js"><\/script>');
+(() => {
+  const script = document.createElement("script");
+  script.src = "https://cdn.jsdelivr.net/gh/5871224/FYH@93370b20954f42d28f0c2666e071d36bf75bd961/src/renderer/v2-attendance-admin.js";
+  script.defer = false;
+  document.head.appendChild(script);
+})();
