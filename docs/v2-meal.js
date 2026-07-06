4202540415733dacfd671a73034a2787dcb501be
5871224 <110716199+5871224@users.noreply.github.com>
2026-07-06 15:54:21 +0800

    Publish V2 meal UI loader

---
+++ docs/v2-meal.js
@@ -0,0 +1,6 @@
+(() => {
+  const script = document.createElement("script");
+  script.src = "https://cdn.jsdelivr.net/gh/5871224/FYH@87ac3325d1b4574556b9ad5afa52c4b73cde64c0/src/renderer/v2-meal.js";
+  script.defer = false;
+  document.head.appendChild(script);
+})();
