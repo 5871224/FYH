bf32993703db975ca18691843cac10f10e4e07e6
5871224 <110716199+5871224@users.noreply.github.com>
2026-07-06 16:01:05 +0800

    Load published V2 attendance admin UI

--- docs/v2-overtime-admin.js
+++ docs/v2-overtime-admin.js
@@ -2,7 +2,8 @@
   [
     "https://cdn.jsdelivr.net/gh/5871224/FYH@462bf7cdf97891d92a7b9021c8d138c0cdd01efa/src/renderer/v2-overtime-admin.js",
     "https://cdn.jsdelivr.net/gh/5871224/FYH@87ac3325d1b4574556b9ad5afa52c4b73cde64c0/src/renderer/v2-meal.js",
-    "https://cdn.jsdelivr.net/gh/5871224/FYH@52f038ca57570ff8b1d4bce1daf5d4e664a90771/src/renderer/v2-account.js"
+    "https://cdn.jsdelivr.net/gh/5871224/FYH@52f038ca57570ff8b1d4bce1daf5d4e664a90771/src/renderer/v2-account.js",
+    "https://cdn.jsdelivr.net/gh/5871224/FYH@93370b20954f42d28f0c2666e071d36bf75bd961/src/renderer/v2-attendance-admin.js"
   ].forEach((src) => {
     const script = document.createElement("script");
     script.src = src;
