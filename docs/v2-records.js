d4f380c50c38159757e698eac5f04099ca7dbf81
5871224 <110716199+5871224@users.noreply.github.com>
2026-07-06 16:13:53 +0800

    Use safe published records loader

--- docs/v2-records.js
+++ docs/v2-records.js
@@ -1,1 +1,6 @@
-document.write('<script src="https://cdn.jsdelivr.net/gh/5871224/FYH@a756bd83da38e391dcba65e1ecb30c45ec54fb10/src/renderer/v2-records.js"><\/script>');
+(() => {
+  const script = document.createElement("script");
+  script.src = "https://cdn.jsdelivr.net/gh/5871224/FYH@a756bd83da38e391dcba65e1ecb30c45ec54fb10/src/renderer/v2-records.js";
+  script.defer = false;
+  document.head.appendChild(script);
+})();
