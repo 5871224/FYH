f1f7520698b6f0f61a7f5994b69a27268fba9f34
5871224 <110716199+5871224@users.noreply.github.com>
2026-08-09 11:49:40 +0800

    依班表實際捲動範圍更新週導覽狀態

--- src/renderer/renderer-schedule-table.js
+++ src/renderer/renderer-schedule-table.js
@@ -83,6 +83,7 @@ function renderTable() {
   syncScheduleColumnWidths();
   renderStickyTableHeader(visibleDates);
   syncScheduleRangeSelectionUi();
+  requestAnimationFrame(syncScheduleWeekNavigationButtons);
 }
 
 function renderHeader() {
